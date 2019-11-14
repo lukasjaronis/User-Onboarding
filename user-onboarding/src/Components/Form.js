157af270ac1c043e7e15b1f1282632fffc2c8954
lukasjaronis <jaronisluke13@gmail.com>
2019-11-14 15:59:48 -0500

    form design 60% done

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,6 +2,38 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+import styled from "styled-components";
+
+
+const FormStyles = styled.div`
+
+.formStyles {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 30rem;
+    height: 30rem;
+    border: 1px solid lightgrey;
+
+    .formLabel {
+        font-size: 1.5rem;
+        font-weight: 900;
+        letter-spacing: 3px;
+        margin: 10px;
+        padding: 5px;
+    }
+
+    .formField {
+        margin: 5px;
+        padding: 5px;
+    }
+    
+}
+
+
+`;
+
 
 
 const OnBoardingForm = ({values, errors, touched, status}) => {
@@ -14,16 +46,17 @@ useEffect( () => {
 
 
     return (
+        <FormStyles>
         <div className="formDiv">
-        <Form>
-        
-        <Field type="text" name="name" placeholder="Name" />
+        <Form className="formStyles">
+        <label className="formLabel">Onboarding</label>
+        <Field className="formField" type="text" name="name" placeholder="Name" />
         {touched.name && errors.name && <p className="errors">{errors.name}</p>}
         
-        <Field type="email" name="email" placeholder="Email" />
+        <Field className="formField" type="email" name="email" placeholder="Email" />
         {touched.email && errors.email && <p className="errors">{errors.email}</p>}
 
-        <Field type="password" name="password" placeholder="Password" />
+        <Field className="formField" type="password" name="password" placeholder="Password" />
         {touched.password && errors.password && <p className="errors">{errors.password}</p>}
 
 
@@ -38,6 +71,7 @@ useEffect( () => {
             </ul>
         ))}
         </div>
+        </FormStyles>
     )
 }
 
